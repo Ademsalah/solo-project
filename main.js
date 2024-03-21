18f82b75fb3a1ac72998d0ac314dfb36f3488b02
brushurteeth8 <adem.hameed.123@gmail.com>
2024-03-21 08:56:47 +0100

    myproj

--- main.js
+++ main.js
@@ -1,34 +1,31 @@
-
-//adding
 function generateId(n) {
   let id = Number(localStorage.getItem("index")) || 0;
   localStorage.setItem("index", (id += 1));
   return (id += 1);
 }
-let container = document.getElementsByClassName("container");
-var coursList = JSON.parse(localStorage.getItem("cours")) || [];
+let container = document.querySelector(".container");
+let coursList = JSON.parse(localStorage.getItem("cours")) || [];
 
-
-
-  let courses = coursList.map(
-    (cours) =>{
-      container.innerHTML+=`<div class="cours" id='${cours.id}>
-      <img src="${cours.Image}" alt="" />
-      <title>${cours.name}</title>
+function render() {
+  let courses = coursList.map((cours) => {
+    return `
+     <div class="cours" id='${cours.id}'>
+      <img src="${cours.Image}" alt="azea" />
+      <h2>${cours.name}</h2>
       <p>${cours.type}</p>
-      <button onclick="deleteMovie(${cours.id})"><i class="fa fa-remove"></i></button>
-      </div>`
-    }
-    )
-    $(".container").append(courses)
-
+      <button onclick="deleteMovie(${cours.id})"></button>
+      </div>
+      `;
+  });
+  container.innerHTML = courses;
+}
 
 $(".add").click(function () {
   var name = $("#inp1").val();
   var img = $("#inp3").val();
   var type = $("#inp2").val();
   var video = $("#inp4").val();
-  
+
   var cours = {
     name: "",
     Image: "",
@@ -40,29 +37,32 @@ $(".add").click(function () {
   cours.type = type;
   cours.video = video;
   cours.id = generateId();
-  
+
   coursList.push(cours);
   localStorage.setItem("cours", JSON.stringify(coursList));
- 
+  render();
 });
 
-  ///side nav 
-  
-  function w3_open() {
-    document.getElementById("main").style.marginLeft = "25%";
-    document.getElementById("mySidebar").style.width = "25%";
-    document.getElementById("mySidebar").style.display = "block";
-    document.getElementById("openNav").style.display = "none";
-  }
-  function w3_close() {
-    document.getElementById("main").style.marginLeft = "0%";
-    document.getElementById("mySidebar").style.display = "none";
-    document.getElementById("openNav").style.display = "inline-block";
-  }
-  function openNav() {
-    document.getElementById("mySidenav").style.width = "25%";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+///side nav
+
+function w3_open() {
+  document.getElementById("main").style.marginLeft = "25%";
+  document.getElementById("mySidebar").style.width = "25%";
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("openNav").style.display = "none";
+}
+function w3_close() {
+  document.getElementById("main").style.marginLeft = "0%";
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("openNav").style.display = "inline-block";
+}
+function openNav() {
+  document.getElementById("mySidenav").style.width = "25%";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+window.addEventListener("DOMContentLoaded", () => {
+  render();
+});
